8296a23c435e8f92027a2814eed083ddf9591751
mihchepi <mihchepi@gmail.com>
2022-01-22 21:20:06 +0000

    broke domain name

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 sed -i -e "s/{DB_USER}/${MYSQL_ROOT}/g" /var/www/wordpress/wp-config.php;
 sed -i -e "s/{DB_PASSWORD}/${MYSQL_ROOT_PASSWORD}/g" /var/www/wordpress/wp-config.php;
-sed -i -e "s/https:\/\/{WP_HOME}/https:\/\/${DOMAIN_NAME}/g" /var/www/wordpress/wp-config.php;
-sed -i -e "s/https:\/\/{WP_SITEURL}/https:\/\/${DOMAIN_NAME}/g" /var/www/wordpress/wp-config.php;
 
 php-fpm7 -F
