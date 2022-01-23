f88eede8be7a166d1153c45f6124920129d812d7
mihchepi <mihchepi@gmail.com>
2022-01-23 07:44:47 +0300

    fix permissions

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -9,11 +9,13 @@ then
     echo "DATABASE ALREADY EXISTS"
 else
     echo "CREATE DATABASE wordpress;" | mysql
-    echo "CREATE USER '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" | mysql
-    echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_USER}'@'%';" | mysql
-    echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%'; IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'" | mysql
-    echo "FLUSH PRIVILEGES;" | mysql
     mysql -u root wordpress < initdb.sql
+    echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" | mysql
+    echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';" | mysql
+    echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_USER}'@'%';" | mysql
+    echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_USER}'@'localhost';" | mysql
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
 fi
 
 rc-service mariadb stop
