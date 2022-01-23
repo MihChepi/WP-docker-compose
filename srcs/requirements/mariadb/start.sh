b46d8761531bb8350e60a82d7f64c3228b15fcc8
Mikhail Chepkin <mikhail.chepkin@arcadia.spb.com>
2022-01-23 03:14:26 +0300

    add database with existing users

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -4,11 +4,16 @@ rc-service mariadb setup
 rc-service mariadb start
 
 # init db
-echo "CREATE DATABASE wordpress;" | mysql
-echo "CREATE USER '${MYSQL_ROOT}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_ROOT}'@'%';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-mysql -u root -p wordpress < initdb.sql
+if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='wordpress'" 2>&1`" ]];
+then
+    echo "DATABASE ALREADY EXISTS"
+else
+    echo "CREATE DATABASE wordpress;" | mysql
+    echo "CREATE USER '${MYSQL_ROOT}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql
+    echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_ROOT}'@'%';" | mysql
+    echo "FLUSH PRIVILEGES;" | mysql
+    mysql -u root wordpress < initdb.sql
+fi
 
 rc-service mariadb stop
 /usr/bin/mysqld_safe
